f8a1a77d82b40240784c219cada62f48373a0152
Natalya Botsman <botsmannatashaa@gmail.com>
2024-09-19 12:46:04 +0600

    Created a Task component, done logic & added css classes

---
+++ src/Components/Task/Task.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './Task.css';
+
+interface ITask {
+    text: string;
+    id: string;
+    onDeleteTask: React.MouseEventHandler;
+}
+
+const Task: React.FC<ITask> = ({text, id, onDeleteTask}) => {
+    return (
+        <div id={id} className="taskContainer">
+            <p className='taskText'>{text}</p>
+            <button type='button' onClick={onDeleteTask} className='taskBtn'>Delete</button>
+        </div>
+    );
+};
+
+export default Task;
