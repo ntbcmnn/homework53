c5a75a144d7371ed163dd173e086fccb6ce9a4fc
Natalya Botsman <botsmannatashaa@gmail.com>
2024-09-19 12:45:18 +0600

    Created a component, done logic & added css classes

---
+++ src/Components/AddTaskForm/AddTaskForm.tsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import './AddTaskForm.css';
+
+interface IAddTaskForm {
+    currentTask: string;
+    onChangeTask: React.ChangeEventHandler<HTMLInputElement>;
+    onAddTask: React.MouseEventHandler;
+}
+
+const AddTaskForm: React.FC<IAddTaskForm> = ({currentTask, onChangeTask, onAddTask}) => {
+    return (
+        <div className='formContainer'>
+            <input type="text"
+                   onChange={onChangeTask}
+                   placeholder="Add Task"
+                   value={currentTask}
+                   className='formInput'
+            />
+            <button type="button" onClick={onAddTask} className='formBtn'>Add</button>
+        </div>
+    );
+};
+
+export default AddTaskForm;
